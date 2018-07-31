2a4163b9d81dacddfd1314389f2970ab7d0c0cfd
Nickolay <nzaharov86@gmail.com>
2018-07-31 19:31:57 +0300

    Add Users

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,9 +1,11 @@
 import Locations from './api/locations/graphql/resovlers';
 import Couriers from './api/couriers/graphql/resolvers';
+import Users from './api/users/graphql/resolvers';
 
 const resolvers = {
   ...Locations,
   ...Couriers,
+  ...Users,
 };
 
 export default resolvers;
