6a4869a582095930c45d0e4a72319db2fd7ac339
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:31:08 +0300

    Add Mutation to resolvers

--- src/api/couriers/graphql/resolvers.js
+++ src/api/couriers/graphql/resolvers.js
@@ -1,9 +1,11 @@
 import types from './types';
 import Query from './queries';
+import Mutation from './mutations';
 
 const resolvers = {
   ...types,
   Query,
+  Mutation,
 };
 
 export default resolvers;
