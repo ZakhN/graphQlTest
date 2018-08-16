491ce152d99c001337f4cd62e1e4453ad5bbc194
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:31:47 +0300

    Add locations find func

--- src/api/couriers/graphql/types.js
+++ src/api/couriers/graphql/types.js
@@ -4,6 +4,10 @@ const types = {
     locations: (root, args, context) => {
       const locations = context.db.locations.filter(loc => root.locations.includes(loc._id));
 
+    locations: async (root, args, context) => {
+      console.log(root.locationIds);
+      const locations = await context.models.Locations.find({ _id: { $in: root.locationIds } });
+      //db.locations.filter(loc => root.locations.includes(loc._id));
       return locations;
     },
   },
