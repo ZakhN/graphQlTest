3ab5db827db7620cf498f3284f67b1ceaf48bd4a
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:32:02 +0300

    Delete locations bd

--- src/api/couriers/graphql/types.js
+++ src/api/couriers/graphql/types.js
@@ -1,9 +1,6 @@
 const types = {
   Courier: {
     fullName: root => `${root.firstName} ${root.lastName}`,
-    locations: (root, args, context) => {
-      const locations = context.db.locations.filter(loc => root.locations.includes(loc._id));
-
     locations: async (root, args, context) => {
       console.log(root.locationIds);
       const locations = await context.models.Locations.find({ _id: { $in: root.locationIds } });
