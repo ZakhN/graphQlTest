8038b6085112f6fb881720ad22d10a12b8bc1656
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:37:17 +0300

    Add import couriers users

--- src/models.js
+++ src/models.js
@@ -1,7 +1,11 @@
 import Locations from './api/locations/model/schema';
+import Couriers from './api/couriers/model/schema';
+import Users from './api/users/model/schema';
 
 const models = {
   Locations,
+  Couriers,
+  Users,
 };
 
 export default models;
