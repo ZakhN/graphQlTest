56758692447bbe5e2f0497d6b53576c0104e667f
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:30:37 +0300

    Add query  couriers

--- src/api/couriers/graphql/queries/couries.js
+++ src/api/couriers/graphql/queries/couries.js
@@ -1,8 +1,11 @@
-const couriers = (root, args, context) => {
-  console.log(context);
-  if (!context.user) throw new Error('Access denied');
 
-  return context.db.couriers;
+const couriersQuery = async (root, args, context) => {
+  console.log(context);
+  // if (!context.user) throw new Error('Access denied');
+
+  const couriers = await context.models.Couriers.find();
+
+  return couriers;
 };
 
-export default couriers;
+export default couriersQuery;
