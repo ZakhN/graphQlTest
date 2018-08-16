8dc013026435f96af9830836509d98bd69d06b82
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:33:01 +0300

    Add schema to couriers model

---
+++ src/api/couriers/model/schema.js
@@ -0,0 +1,20 @@
+const autoIncrement = require('mongoose-auto-increment');
+const mongoose = require('../../../core/connection');
+
+const mongooseCon = mongoose.createConnection('mongodb://localhost:27017/test', { useNewUrlParser: true });
+autoIncrement.initialize(mongooseCon);
+
+const couriersSchema = new mongoose.Schema({
+  // fullname: String,
+  // locations: Object,
+  firstName: String,
+  lastName: String,
+  status: String,
+  locationIds: [String],
+});
+
+const couriersModel = mongoose.model('couriers', couriersSchema);
+
+couriersSchema.plugin(autoIncrement.plugin, 'couriers');
+
+export default couriersModel;
