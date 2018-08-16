6ea46aa858db666536e92174c7399ad173952de5
Nickolay <nzaharov86@gmail.com>
2018-08-16 19:36:29 +0300

    Add connect

--- src/core/connection.js
+++ src/core/connection.js
@@ -1,11 +1,15 @@
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
 mongoose.connection.on('error', (err) => {
   console.error(`Database Connection Error:  + ${err}`);
 });
+
+
 mongoose.connection.on('connected', () => {
   console.info('Succesfully connected to MongoDB Database');
 });
+
 
 module.exports = mongoose;
