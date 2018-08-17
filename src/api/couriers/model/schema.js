a75680008f9dd7c2c4be93e1cb510b19a6f688c6
Nickolay <nzaharov86@gmail.com>
2018-08-17 17:52:16 +0300

    Update couriers schema

--- src/api/couriers/model/schema.js
+++ src/api/couriers/model/schema.js
@@ -1,13 +1,6 @@
-const autoIncrement = require('mongoose-auto-increment');
-const mongoose = require('../../../core/connection');
-
-const mongooseCon = mongoose.createConnection('mongodb://localhost:27017/test', { useNewUrlParser: true });
-autoIncrement.initialize(mongooseCon);
 import { autoIncrement, mongoose } from '../../../core/connections/mongo';
 
 const couriersSchema = new mongoose.Schema({
-  // fullname: String,
-  // locations: Object,
   firstName: String,
   lastName: String,
   status: String,
