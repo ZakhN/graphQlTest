0457e24397fd400711dc5c4e1f2032d59c3c9f64
Nickolay <nzaharov86@gmail.com>
2018-08-17 17:49:05 +0300

    Delete old vadidation

--- src/api/couriers/graphql/queries/couries.js
+++ src/api/couriers/graphql/queries/couries.js
@@ -1,8 +1,5 @@
-
 const couriersQuery = async (root, args, context) => {
   console.log(context);
-  // if (!context.user) throw new Error('Access denied');
-
   const couriers = await context.models.Couriers.find();
 
   return couriers;
