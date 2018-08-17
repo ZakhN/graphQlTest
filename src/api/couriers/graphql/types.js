bcb50c90c0f93aecf9fc4be87cf3a7ae8be517f9
Nickolay <nzaharov86@gmail.com>
2018-08-17 17:49:22 +0300

    Delete console log

--- src/api/couriers/graphql/types.js
+++ src/api/couriers/graphql/types.js
@@ -2,9 +2,7 @@ const types = {
   Courier: {
     fullName: root => `${root.firstName} ${root.lastName}`,
     locations: async (root, args, context) => {
-      console.log(root.locationIds);
       const locations = await context.models.Locations.find({ _id: { $in: root.locationIds } });
-      //db.locations.filter(loc => root.locations.includes(loc._id));
       return locations;
     },
   },
