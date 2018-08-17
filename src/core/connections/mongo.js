3a0900845c5f8665c20af1ee702815fd9a1539d4
Nickolay <nzaharov86@gmail.com>
2018-08-17 17:51:46 +0300

    Update core

--- src/core/connections/mongo.js
+++ src/core/connections/mongo.js
@@ -1,15 +1,15 @@
 
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
 
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+autoIncrement.initialize(mongoose);
+
 mongoose.connection.on('error', (err) => {
   console.error(`Database Connection Error:  + ${err}`);
 });
-
-
 mongoose.connection.on('connected', () => {
   console.info('Succesfully connected to MongoDB Database');
 });
 
-
-module.exports = mongoose;
+export { mongoose, autoIncrement };
