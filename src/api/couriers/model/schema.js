c371b34e7850fcc073ad18f5356d73a3c2f2f353
Nickolay <nzaharov86@gmail.com>
2018-08-17 17:49:52 +0300

    Add connections

--- src/api/couriers/model/schema.js
+++ src/api/couriers/model/schema.js
@@ -3,6 +3,7 @@ const mongoose = require('../../../core/connection');
 
 const mongooseCon = mongoose.createConnection('mongodb://localhost:27017/test', { useNewUrlParser: true });
 autoIncrement.initialize(mongooseCon);
+import { autoIncrement, mongoose } from '../../../core/connections/mongo';
 
 const couriersSchema = new mongoose.Schema({
   // fullname: String,
